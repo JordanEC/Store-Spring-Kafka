2a1f36911b4b11a251b7687222c8dfe280cf41d4
Jordan Espinoza <jordan47ec@gmail.com>
2020-11-23 11:22:46 -0600

    Create Kubernates deployments and services for React app

--- reBuildServices.sh
+++ reBuildServices.sh
@@ -6,7 +6,7 @@ echo "****************"
 echo "****************"
 echo "producer: docker build..."
 echo "****************"
-docker build -t jordanec/store-producer:latest producer/.
+docker build -t jordanec/store-producer:latest -f producer/Dockerfile ./producer/
 echo "****************"
 echo "producer: build done!"
 echo "****************"
@@ -17,7 +17,22 @@ echo "****************"
 echo "****************"
 echo "consumer: docker build..."
 echo "****************"
-docker build -t jordanec/store-consumer:latest consumer/.
+docker build -t jordanec/store-consumer:latest -f consumer/Dockerfile ./consumer/
 echo "****************"
 echo "consumer: build done!"
+echo "****************"
+echo "****************"
+echo "store-web: docker build..."
+echo "****************"
+docker build -t jordanec/store-web:latest -f store-web/Dockerfile-dev ./store-web/
+echo "****************"
+echo "store-web: build done!"
+echo "****************"
+echo "****************"
+echo "store-web (Prod): docker build..."
+echo "****************"
+docker build -t jordanec/store-web-prod:latest -f store-web/Dockerfile ./store-web/
+#docker build -f store-web/Dockerfile -t jordanec/store-web-prod:latest store-web/.
+echo "****************"
+echo "store-web (Prod): build done!"
 echo "****************"
